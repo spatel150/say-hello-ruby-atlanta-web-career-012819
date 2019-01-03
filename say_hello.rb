cdeb350ff89e04d276c8e4c4b5679cdf206aa767
Samidh Patel <spatel13456@gmail.com>
2019-01-03 23:45:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,14 @@
 # Build your say_hello method here
+def say_hello(name) 
+  puts "Hello #{name}!"
+end 
+
+say_hello("Kent Beck")
+
+def say_hello(name, language = "Ruby Progammer") 
+  puts "Hello #{langauge}!"
+end 
+
+
+
+
