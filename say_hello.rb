509177d536e472f62f35be5fed252ac19c07193a
Samidh Patel <spatel13456@gmail.com>
2019-01-03 23:59:50 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,13 +3,6 @@ def say_hello(name)
   puts "Hello #{name}!"
 end 
 
-say_hello("Kent Beck")
-
-def say_hello(name, language = "Ruby Progammer") 
-  puts "Hello #{langauge}!"
-end 
-
-say_hello(language)
 
 
 
